d7a1427b4fd941ccca3afcc39f3316fc507e7175
Zebaro24 <69474746+Zebaro24@users.noreply.github.com>
2022-07-10 16:04:08 +0300

    Main files

---
+++ main.py
@@ -0,0 +1,9 @@
+import main_discord
+import main_telegram
+
+def start_bots():
+    pass
+
+
+if __name__ == '__main__':
+    start_bots()
---
+++ main_telegram.py
@@ -0,0 +1,5 @@
+import telebot
+
+
+if __name__ == '__main__':
+    pass
