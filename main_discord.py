591c3b2826dc7aa1a407be9d272a470a4926f69c
WhiteDh <100050383+WhiteDh@users.noreply.github.com>
2022-07-11 17:32:41 +0300

    unfinished roles

--- main_discord.py
+++ main_discord.py
@@ -1,15 +1,14 @@
 import discord
-import main_constants
+import config
+from discord import utils
 from discord.ext import commands
 from discord.message import Message
-
+import roles
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
     print(f"Bot was started: {client.user}")
-
 
 # Все ивенты: https://discordpy.readthedocs.io/en/latest/api.html#event-reference
 @client.event
@@ -18,10 +17,9 @@ async def on_message(message: Message):
     # Чтобы бот не считывал свои сообщения
     if message.author == client.user:
         return
-
     # Не забывать await
-    await message.channel.send("sdsdsds")  # Отправить в канал
-    await message.author.send("sasasa")  # Отправить в личку
+    #await message.channel.send("sdsdsds")  # Отправить в канал
+    #await message.author.send("sasasa")  # Отправить в личку
 
     print(message.author)
     channel: discord.TextChannel = client.get_channel(995704829416583200)  # Канал по id
@@ -35,5 +33,23 @@ async def on_message(message: Message):
         await message.channel.send('Hello!')
 
 
+# roles
+@client.event
+async def on_raw_reaction_add(payload):
+    channel = client.get_channel(payload.channel_id)
+    message = await channel.fetch_message(payload.message_id)
+    member = utils.get(message.guild.members, id=payload.user_id)
+
+    print(member)
+    try:
+        emoji = str(payload.emoji)
+        print(emoji)
+        role = utils.get(message.guild.members, id=roles.ROLES[emoji])
+        print(role)
+        await member.add_roles(role)
+    except:
+        print("net roli")
+
 if __name__ == '__main__':
-    client.run(main_constants.DISCORD_API)
+    client.run(config.DISCORD_API)
+
--- main_telegram.py
+++ main_telegram.py
@@ -1,5 +1,5 @@
 import telebot
-import main_constants
+import config
 
 if __name__ == '__main__':
     pass
