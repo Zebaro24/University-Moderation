333c0dbe869b3c97cdb2ddcae4abd9e28deec61f
Zebaro24 <69474746+Zebaro24@users.noreply.github.com>
2022-07-15 00:49:38 +0300

    Telegram message to discord 1.1

--- telegram_bot/main.py
+++ telegram_bot/main.py
@@ -1,8 +1,13 @@
 from telebot import TeleBot
 import config
 
+import discord_bot.main_discord
+
 bot = TeleBot(config.TELEGRAM_API)
 
+@bot.message_handler(content_types=["text"])
+def telega_to_ds(message):
+    discord_bot.main_discord.bot.get_guild(config.discord_guild).get_chanell("997248600960680058").send(message.text)
 
 def start():
     bot.polling()
