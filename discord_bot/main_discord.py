618f5e32a92acab0fca9e91dc40ded889beedc3c
Zebaro24-Stud <zebaro24@stu.cn.ua>
2022-08-28 18:46:33 +0300

    Красивое оформление

--- discord_bot/main_discord.py
+++ discord_bot/main_discord.py
@@ -1,43 +1,39 @@
 # Основные дискорд библиотеки
-
 import discord
 from discord.ext import commands
 import wavelink
-from wavelink.ext import spotify
-from config import client_id, client_secret, music_channel_id
+from config import music_channel_id
+
 # Для слеш команд используем dislash
 from dislash import InteractionClient
 from asyncio import sleep
-# Для кнопок и тд используем discord_components
-# from discord_components import DiscordComponents - Убран
 
 # Конфиги и доп библиотеки
 from utils import print_ds, bc
 from config import DISCORD_API, ds_chanel_id, discord_guild, mafia_channel_id
 
-inst: discord.flags.Intents = discord.Intents.all()
-bot = commands.Bot("!", intents=inst)
 # https://dislashpy.readthedocs.io/en/latest/quickstart.html#creating-a-simple-command - Slash command
+# Основные классы бота
+bot = commands.Bot("!", intents=discord.Intents.all())
 slash = InteractionClient(bot)
 
 # Возможности
-import discord_bot.discord_button
-import discord_bot.roles.roles_commands as roles
-import tg_ds.ds_to_tg as ds_to_tg
-import discord_bot.mafia.mafia_start as mafia_start
 from discord_bot.music.music_message import update_message
-from discord_bot.music.music_commands import playlist, read_url, play
-from discord_bot.create_voice.create import delete_excess
 from discord_bot.control_version import check_version
-import discord_bot.music.music_commands
+import discord_bot.roles.roles_commands as roles
+import discord_bot.create_voice as create_voice
+import discord_bot.mafia.mafia_start as mafia_start
+import discord_bot.music.music_commands as music_commands
+import tg_ds.ds_to_tg as ds_to_tg
+import discord_bot.discord_button
 import discord_bot.activity
 import discord_bot.mafia.mafia_menu
-import discord_bot.create_voice.create
 import discord_bot.decor_message
 import discord_bot.help_command
 import discord_bot.rgb_lenta
 
 
+# При готовности бота
 @bot.event
 async def on_ready():
     print_ds(f"Бот был инициализирован под именем: {bot.user.name}")
@@ -57,7 +53,7 @@ async def on_ready():
     await mafia_start.mafia_start(guild.get_channel(mafia_channel_id))
 
     print_ds("Удаление созданных голосовых каналов")
-    await delete_excess(guild)
+    await create_voice.delete_excess(guild)
 
     bot.loop.create_task(start_wavelink())
 
@@ -67,6 +63,7 @@ async def on_ready():
     print_ds(f"Бот был {bc('01;38;05;34')}запущен{bc()}")
 
 
+# Запуск музыкальной ноды
 async def start_wavelink():
     await sleep(2)
     connect = False
@@ -76,12 +73,14 @@ async def start_wavelink():
         await sleep(5)
 
 
+# Нода готова
 @bot.event
 async def on_wavelink_node_ready(node: wavelink.Node):
     print_ds(f"Музыкальная нода была запущена под идентификатором: {node.identifier}")
 
 
 # Все ивенты: https://discordpy.readthedocs.io/en/latest/api.html#event-reference
+#
 @bot.event
 async def on_message(message: discord.Message):
     # Если автор совпадает с клиентом – то вернуть
@@ -96,11 +95,11 @@ async def on_message(message: discord.Message):
 
     # Взаимодействие в муз канале
     if music_channel_id == message.channel.id:
-        if playlist:
-            await read_url(message.content)
+        if music_commands.playlist:
+            await music_commands.read_url(message.content)
             await message.reply("Музон был добавлен", delete_after=3)
         else:
-            await play(message, message.content)
+            await music_commands.play(message, message.content)
         await message.delete()
 
     if "clear" == message.content:
@@ -110,6 +109,7 @@ async def on_message(message: discord.Message):
         pass
 
 
+# Старт бота
 def start():
     bot.run(DISCORD_API)
 
