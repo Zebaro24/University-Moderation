99f850bdf70e415c826af1d30abce478cd4f5f75
Zebaro24 <zebaro24@stu.cn.ua>
2024-04-06 16:11:50 +0300

    Видалені проекти які запускалися разом з основним проектом

--- other/IE_101/bot.py
+++
@@ -1,6 +0,0 @@
-import telebot
-from os import getenv
-
-TOKEN = getenv("IE_101_TOKEN")
-
-bot = telebot.TeleBot(TOKEN)
--- worker.py
+++ worker.py
@@ -1,11 +1,7 @@
-from .config import debug
 from .utils import bc, set_logger, info, time_start_bot
 
 from .discord_bot.main_discord import start as start_discord
 from .telegram_bot.main_telegram import start as start_telegram
-
-from .other.IE_101.main_telegram import start as start_dima_bot
-from .other.feit_bot import start_feit_bot
 
 from .database_func import load_all_elements
 
@@ -32,9 +28,6 @@ class Worker:
         locale.setlocale(locale.LC_ALL, "uk_UA")
 
         Thread(target=start_telegram, daemon=True).start()
-        if not debug:
-            Thread(target=start_feit_bot, daemon=True).start()
-            Thread(target=start_dima_bot, daemon=True).start()
         start_discord()
 
         print(f"{bc(32)}<---Программа была остановлена--->{bc()}")
